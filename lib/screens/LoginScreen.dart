a10e8f9dc2fe0c69cec38a384bb907d54d23a7fd
5thfader <chrisjeremy84@live.com>
2022-05-28 01:39:57 +0200

    Registration & Login UI done [IOS, Android]

---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'RegistrationScreen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  // The following variables are required;
+  //form key
+  //emailController , passwordController
+  final _formKey = GlobalKey<FormState>();
+
+  //editing controller
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your email!");
+        }
+        // reg expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+
+    //passwordField
+    final passwordField = TextFormField(
+        autofocus: false,
+        controller: passwordController,
+        obscureText: true,
+        validator: (value) {
+          RegExp regexp = new RegExp(r'^.{6,}$');
+          if (value!.isEmpty) {
+            return ("Password is required for login");
+          }
+          if (!regexp.hasMatch(value)) {
+            return ("Please Enter Valid PAssword(Min. 6 Character)");
+          }
+        },
+        onSaved: (value) {
+          emailController.text = value!;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.vpn_key_sharp),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Password",
+            hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+            border:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(10))));
+
+    //loginButton
+    //Login Function at the bottom
+    final loginButton = Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.indigoAccent,
+        child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+
+          },
+          child: Text("Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
+        ));
+
+
+
+
+
+
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                    child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                //PADDING
+                                SizedBox(height: 45),
+                                //WELCOME TO ZYMEE.
+                                const SizedBox(
+                                  height: 40,
+                                  child: Text(
+                                    "Login to Zymee.",
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        color: Colors.blueGrey,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+
+                               const SizedBox(height: 45),
+                                emailField,
+                               const  SizedBox(height: 25),
+                                passwordField,
+                               const SizedBox(height: 35),
+                                loginButton,
+                               const SizedBox(height: 15),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                   const Text("Don't have an account? "),
+                                    GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    RegistrationScreen()));
+                                      },
+                                      child: const Text("SignUp",
+                                          style: TextStyle(
+                                              color: Colors.indigoAccent,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: (15))),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )))),
+              )))
+    );
+  }
+}
---
+++ lib/screens/RegistrationScreen.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  //The following variables are required
+
+  //FormKey
+  final _formKey = GlobalKey<FormState>();
+
+  //Editing controllers
+  final fullNameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final phoneNumberEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    //Full Name Field
+    final fullNameField = TextFormField(
+      autofocus: false,
+      controller: fullNameEditingController,
+      keyboardType: TextInputType.name,
+      validator: (value) {
+        RegExp regexp = new RegExp(r'^.{3,}$');
+        if (value!.isEmpty) {
+          return ("Full Name cannot be empty");
+        }
+        if (!regexp.hasMatch(value)) {
+          return ("Enter Valid name(min. 3 Characters)");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        fullNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.portrait_sharp),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Full name",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    //email field
+    final emailTextField = TextFormField(
+      autofocus: false,
+      controller: emailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your email!");
+        }
+        // reg expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        emailEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    //Telephone field
+    final phoneNumberField = TextFormField(
+      autofocus: false,
+      controller: phoneNumberEditingController,
+      keyboardType: TextInputType.text,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Field cannot be empty");
+        }
+        const pattern = r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$';
+        final regExp = RegExp(pattern);
+        if (!regExp.hasMatch(value)) {
+          return ("Number does not meet requirements");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        phoneNumberEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.call),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Phone Number",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    //Password
+    final passwordField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: passwordEditingController,
+      //Keyboard type not required for pasword.
+      validator: (value) {
+        RegExp regexp = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regexp.hasMatch(value)) {
+          return ("Please Enter Valid PAssword(Min. 6 Character)");
+        }
+      },
+      onSaved: (value) {
+        passwordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key_sharp),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    //Confirm Password
+    final confirmPasswordField = TextFormField(
+      autofocus: false,
+      controller: confirmPasswordEditingController,
+      obscureText: true,
+      validator: (value) {
+        if (confirmPasswordEditingController.text !=
+            passwordEditingController.text) {
+          return "Password don't match";
+        }
+        return null;
+      },
+      onSaved: (value) {
+        confirmPasswordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key_sharp),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Confirm Password",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+
+    //loginButton
+    final signUpButton = Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.indigoAccent,
+        child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+
+          },
+          child: Text("SignUp",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
+        ));
+
+
+
+
+    return  MaterialApp(
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+          appBar: AppBar(
+            actions: [],
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_sharp, color: Colors.indigoAccent),
+              onPressed: () {
+                //Passing this to our root.
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          body: Center(
+              child: SingleChildScrollView(
+                child: Container(
+                    child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                //PADDING
+                                SizedBox(height: 35),
+
+                                //WELCOME TO ZYMEE.
+                               const SizedBox(
+                                  height: 20,
+                                  child: Text(
+                                    "Just a few steps",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.blueGrey,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+
+                               const SizedBox(height: 25),
+                                fullNameField,
+                               const SizedBox(height: 25),
+                                emailTextField,
+                               const SizedBox(height: 25),
+                                phoneNumberField,
+                               const SizedBox(height: 25),
+                                passwordField,
+                               const SizedBox(height: 25),
+                                confirmPasswordField,
+                               const SizedBox(height: 25),
+                                signUpButton,
+                               const SizedBox(height: 25),
+                              ],
+                            )))),
+              ))),
+    );
+  }
+}
