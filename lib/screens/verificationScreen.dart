cab4602063d5c0249c8c8b1fad144952467d57c5
5thfader <chrisjeremy84@live.com>
2022-06-13 20:07:20 +0200

    SUCCESSFULL BUILD:  THIS VERSION HAS BEEN APPROVED BY SUPERVISOR, HOWEVER. \n This version has been presented to the supervisor and has been approved. \n The only notes that have been made were these: \n - FIX GOOGLE API: GOOGLE API Not functioning and currently the error points at error 400. \n CHANGE CONTACT LOGIC: Supervisor request the change of the call functionality. Messages such as\n SMS must be used instead to share the meething link allowing with the generated room. \n The Complete aproval of the project shall be achieved after this.

--- lib/screens/verificationScreen.dart
+++ lib/screens/verificationScreen.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:zymee/screens/HomeScreen.dart';
 import 'package:zymee/screens/SetUpScreen.dart';
-
+import 'package:pin_code_fields/pin_code_fields.dart';
 import '../modals/userModals.dart';
 
 class VerificationScreen extends StatefulWidget {
@@ -18,9 +20,15 @@ class VerificationScreen extends StatefulWidget {
 
 class _VerificationScreenState extends State<VerificationScreen> {
 
+  late String _verificationCode;
   bool isVerified = false;
   bool canResend = false;
   Timer? timer;
+
+  final TextEditingController phoneNumber = TextEditingController();
+
+
+
 
   @override
   void initState() {
@@ -36,6 +44,7 @@ class _VerificationScreenState extends State<VerificationScreen> {
       );
     }
   }
+
 
 
 
@@ -81,6 +90,65 @@ class _VerificationScreenState extends State<VerificationScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+
+
+    final phoneNumberField = TextFormField(
+      autofocus: false,
+      controller: phoneNumber,
+      keyboardType: TextInputType.number,
+      validator: (value) {
+        RegExp regexp = new RegExp(r'^.{3,}$');
+        if (value!.isEmpty) {
+          return ("Full Name cannot be empty");
+        }
+        if (!regexp.hasMatch(value)) {
+          return ("Enter Valid name(min. 3 Characters)");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        phoneNumber.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIconColor: Colors.white,
+          filled: true,
+          fillColor: Colors.orangeAccent,
+          prefixIcon: Icon(Icons.phone_android),
+          prefixText: "+48",
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Phone number",
+          hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+
+
+    final phoneVerificationButton = Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.orangeAccent,
+        child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+            _verifyPhone();
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => isPhoneVerification(context, phoneNumber.text))
+              );
+          },
+          child: Text("Verify",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
+        ));
+
+
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Verification Screen",
@@ -96,6 +164,7 @@ class _VerificationScreenState extends State<VerificationScreen> {
       backgroundColor: Colors.white,
       body: Center(
           child: widget.isEmail == true ?
+              //Rendering for email verification
           Container(
             child: Column(
               children: [
@@ -116,15 +185,147 @@ class _VerificationScreenState extends State<VerificationScreen> {
               ],
             ),
           )
+                //Rendering phone verifications
+              :
+          Padding(padding: EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+                color: Colors.blueGrey.shade400
+          ),
+              child: Center(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 35.0),
+                    Padding(
+                      padding: const EdgeInsets.all(28.0),
+                      child: const Text("Please enter verification number", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.white),),
+                    ),
+                    const SizedBox(height: 35.0),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: phoneNumberField,
+                    ),
+                    const SizedBox(height: 46.0),
+                    phoneVerificationButton
+                  ],
+                ),
+              ),
+          ),
+          )
 
-              : Text(
-              "Verification message sent -> ${widget.isEmail}")
+      ),
+    );
+  }
+
+  
+
+
+  //Phone Verification Widget
+    //The phone verification widget Adds the pin code verification on to the screen.
+    //As built in functions work well with our goal, once the pincode completed our user will be authenticated... if the right code is added.
+  Widget isPhoneVerification(BuildContext context,String phone){
+
+    final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
+  
+    final TextEditingController _pinPutController = TextEditingController();
+    final FocusNode _pinPutFocusNode = FocusNode();
+
+
+
+
+
+    return Scaffold(
+      appBar: AppBar(title: Text("CODE SENT", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),),),
+      body: Container(
+        child: Column(
+
+          children: [
+            const SizedBox(height: 25.0),
+            const Text("A code has been sent to your number."),
+            const SizedBox(height: 25.0),
+            Container(child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: PinCodeTextField(
+                appContext: context,
+                length: 6,
+                obscureText: false,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(5),
+                  fieldHeight: 50,
+                  fieldWidth: 40,
+                  activeFillColor: Colors.white,
+                ),
+                animationDuration: Duration(milliseconds: 300),
+                onCompleted: (v) async{
+                  try{
+                    await FirebaseAuth.instance
+                        .signInWithCredential(PhoneAuthProvider.credential(verificationId: _verificationCode, smsCode: v))
+                        .then((value) async{
+                       if(value.user != null){
+                         print("User Phone authenticated");
+                         isVerified = true;
+                         postToUserCollection(isVerified);
+                         Fluttertoast.showToast(msg: "Phone authentication Successfull \nCreating User model");
+                          Navigator.pushReplacement(context, 
+                          MaterialPageRoute(builder: (context) => HomeScreen()));
+                       }
+                    });
+                  }catch(e){
+                    FocusScope.of(context).unfocus();
+                    _scaffoldkey.currentState!;
+                      Fluttertoast.showToast(msg: "Invalid OTP");
+                  }
+                },
+                onChanged: (v){
+                  print(v);
+                  setState(() {});
+                },
+              ),
+            ))
+          ],
+        ),
       ),
     );
   }
 
 
 
+
+  //Phone verification method
+    //This method is called out to invoke the sending of the message to the number provided.
+      _verifyPhone() async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: "+48${phoneNumber.text}",
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await FirebaseAuth.instance
+              .signInWithCredential(credential)
+              .then((value) async {
+
+          });
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          print(e.message);
+        },
+        codeSent: (String verficationID, int? resendToken) {
+          setState(() {
+            _verificationCode = verficationID;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationID) {
+          setState(() {
+            _verificationCode = verificationID;
+          });
+        },
+        timeout: Duration(seconds: 120));
+
+  }
+
+  //Post to user collection as of now is only ran once the verification is complete for email
+  // Must implement verifications for phone number as well
+  //IT is the element that updates the verification method on our cloud firebase database.
   postToUserCollection(bool isVerified) async {
     //calling our firestore
     //calling our user model
